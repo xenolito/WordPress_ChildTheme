54dfcb4ca7c60b8866c7b3e0b3036339c50f0aba
xenolito <oscar.rey.tajes@gmail.com>
2020-07-20 18:26:50 +0200

    various tweaks

--- functions.php
+++ functions.php
@@ -4,12 +4,17 @@ function oceanwp_child_enqueue_parent_style() {
 	$theme   = wp_get_theme( 'OceanWP' );
 	$version = $theme->get( 'Version' );
 	// Load the stylesheet
-	wp_enqueue_style( 'pictau-child', get_stylesheet_directory_uri() . '/style.css', array( 'osh-styles' ), $version );
+	wp_enqueue_style( 'pictau-child', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
 
 }
-add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );/* custom PHP functions below this line */
+add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style',100 );/* custom PHP functions below this line */
 
+add_action( 'wp_enqueue_scripts', 'remove_default_stylesheet', 20 );
+function remove_default_stylesheet() {
+	wp_deregister_style( 'elementor-icons' );
+	//wp_deregister_style( 'simple-line-icons' ); // used by oceanwp like search magnifier and home icon on header breadcrumb...
 
+}
 /*------------------------------------------------------------------------------------------------------*\
 
 						WP MAIL FROM CUSTOMIZATION
@@ -21,7 +26,7 @@ function custom_wp_mail_from_name( $original_email_from ) {
 }
 
 function custom_wp_email_address( $original_email_address ) {
-    return 'contacto@sejimenez.com';
+    return 'info@sejimenez.com';
 }
 
 
@@ -213,7 +218,215 @@ function google_map_api_key_fix(){
 add_action('wp_enqueue_scripts', 'google_map_api_key_fix');
 
 
+/*------------------------------------------------------------------------------------------------------*
+						MOBILE MENU WIDGET
+\*------------------------------------------------------------------------------------------------------*/
+function mobile_menu_widgets_init() {
+ register_sidebar( array(
+ 'name' => 'Mobile Menu Widget',
+ 'id' => 'widget_area' ) );
+}
+add_action( 'widgets_init', 'mobile_menu_widgets_init' );
 
+
+
+
+add_filter( 'excerpt_more', function( $more ) {
+	return '...';
+}, 25 );
+
+
+
+/**
+ * CHANGE THE "page" slug for pagination
+ *
+ */
+
+function re_rewrite_rules() {
+global $wp_rewrite;
+$wp_rewrite->pagination_base = esc_attr__( 'page', 'oceanwp' );
+$wp_rewrite->flush_rules();
+}
+add_action('init', 're_rewrite_rules');
+
+
+/*------------------------------------------------------------------------------------------------------*
+						SHORTCODE FOR RECRUSOS DESCARGABLES
+\*------------------------------------------------------------------------------------------------------*/
+
+
+function recurso_pasti($atts, $content = null) {
+
+    extract(shortcode_atts(array(
+	"title"		=> '',
+	"img"		=> '',
+	"ribbon"	=> '',
+	"form"	=> '',
+	), $atts));
+
+	$ribbonFront = ($ribbon) ? '<div class="card-ribbon"><span>'. $ribbon .'</span></div>' : '';
+	$ribbonBack = ($ribbon) ? '<div class="card-ribbon-back"><span></span></div>' : '';
+	$imgOut = '<img src="' . do_shortcode('[myPics]') . $img . '" />';
+	$loquiero = esc_attr__( 'Lo quiero', 'oceanwp' );
+
+	$html_out = '<card class="pct-recurso-tile">
+		<div class="flip-card">
+			<div class="flip-card-front">';
+
+	$html_out.= $ribbonFront;
+
+	$html_out.= '<div class="card-content">
+					<div class="card-header">
+						<figure>'.  $imgOut .'</figure>
+						<h2>'. $title .'</h2>
+					</div>
+					<div class="card-description">
+						<p>'. $content .'</p>
+						<a class="button elementor-animation-grow" href="#">
+							<span>'. $loquiero .'</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="flip-card-back">';
+	$html_out.= $ribbonBack;
+
+	$html_out.= '<div class="pct-close-flipped"><a href="#"><i class="sidr-class-icon icon-close" aria-hidden="true"></i></a></div>';
+
+	$html_out.= '<div class="pct-lead-heading"><h2 class="elementor-heading-title elementor-size-default">Te lo envío a tu email</h2>
+<p>
+Indícame tu email y recibirás el contenido cómodamente en tu buzón.</p></div>';
+
+	$html_out.= do_shortcode('[contact-form-7 id="'. $form .'" title="magnet-descargable" html_class="submit_on_email"]');
+
+	$html_out.= '</div>
+		</div>
+	</card>';
+
+	return $html_out;
+}
+
+add_shortcode('recurso', 'recurso_pasti');
+
+
+
+/*------------------------------------------------------------------------------------------------------*
+						LEARNDASH + ELEMENTOR
+\*------------------------------------------------------------------------------------------------------*/
+
+/**
+ * If running Elementor Pro 2.3.0 of higher there is a filter to add custom post types
+ */
+add_filter( 'elementor_pro/utils/get_public_post_types', function( $post_types ) {
+	$post_types['sfwd-courses'] = LearnDash_Custom_Label::get_label( 'courses' );
+	$post_types['sfwd-lessons'] = LearnDash_Custom_Label::get_label( 'lessons' );
+	$post_types['sfwd-topic'] = LearnDash_Custom_Label::get_label( 'topics' );
+	$post_types['sfwd-quiz'] = LearnDash_Custom_Label::get_label( 'quizzes' );
+
+	return $post_types;
+} );
+
+
+
+/*------------------------------------------------------------------------------------------------------*
+						SHORTCODE FOR COURSE LANDING CONDITIONAL (VISTOR vs ENROLLED)
+\*------------------------------------------------------------------------------------------------------*/
+
+
+function landing_curso_content($atts, $content = null) {
+
+    extract(shortcode_atts(array(
+	"template"		=> '',
+	), $atts));
+
+	return  '<div class="pct-visitor-content">' . do_shortcode('[visitor]'. do_shortcode('[elementor-template id="'. $template .'"]') .'[/visitor]') . '</div>';
+}
+
+add_shortcode('landing_curso', 'landing_curso_content');
+
+
+
+function ld_student_pictau($atts,$content){
+	$text = do_shortcode('[ld_profile]');
+
+
+	$final = do_shortcode('[student]'. $text . $content . '[/student]');
+	$final = strip_tags($final,'<div><ul><li><i><a><table><td><th><tr><style><img><span><form><input><strong><h2>');
+
+	return $final;
+}
+
+add_shortcode('student_pct','ld_student_pictau');
+
+/*------------------------------------------------------------------------------------------------------*
+						WOOCOMMERCE MI CUENTA UNIFICAMOS PESTAÑAS DIRECCIONES Y EDITAR CUENTA
+\*------------------------------------------------------------------------------------------------------*/
+
+/* Unificando contenido de pestañas */
+// Primero ocultamos la pestaña a mover (edit-address en este ejemplo)
+
+add_filter( 'woocommerce_account_menu_items', 'mover_micuenta_direcciones', 999 );
+
+function mover_micuenta_direcciones( $items ) {
+unset($items['edit-address']);
+return $items;
+}
+
+// Luego mostramos el contenido de las direcciones en otra pestaña (edit-account en este ejemplo)
+
+add_action( 'woocommerce_account_edit-account_endpoint', 'woocommerce_account_edit_address' );
+
+
+
+/*------------------------------------------------------------------------------------------------------*
+						REMOVE AUTO <p> + <br> tags on do_shortcodes  --> wpautop
+						How it works: We disable wpautop so do_shortcode execs "before", then we re-enable it with a lower priority (11)
+\*------------------------------------------------------------------------------------------------------*/
+//move wpautop filter to AFTER shortcode is processed
+//remove_filter( 'the_content', 'wpautop' );
+//remove_filter('the_content', 'wptexturize');
+//add_filter( 'the_content', 'wpautop' , 999);
+
+
+
+
+/*
+add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
+
+	function add_current_nav_class($classes, $item) {
+
+		// Getting the current post details
+		global $post;
+
+		// Getting the post type of the current post
+		$current_post_type = get_post_type_object(get_post_type($post->ID));
+		$current_post_type_slug = $current_post_type->rewrite[slug];
+
+		// Getting the URL of the menu item
+		$menu_slug = strtolower(trim($item->url));
+
+		// If the menu item URL contains the current post types slug add the current-menu-item class
+		if (strpos($menu_slug,$current_post_type_slug) !== false) {
+
+		   $classes[] = 'current-menu-item';
+
+		}
+
+		// Return the corrected set of classes to be added to the menu item
+		return $classes;
+
+	}
+*/
+
+/**
+ * Change the Related Posts title "You Might Also Like" ....
+ */
+/*
+ function myprefix_related_posts_title() {
+	return esc_attr__( 'You might also like', 'oceanwp' );
+}
+add_filter( 'ocean_related_posts_title', 'myprefix_related_posts_title' );
+*/
 
 
 ?>
--- js/custom_theme_scripts.js
+++ js/custom_theme_scripts.js
@@ -1,13 +1,16 @@
 (function ($) {
-  "use strict";
+  ("use strict");
 
   /*------------------------------------------------------------------------------------------------------*\
 
-							ONLOAD SECTION
+							ONLOAD
 
 	\*------------------------------------------------------------------------------------------------------*/
 
   $(window).on("load", function () {
+    $("html").addClass("pct-ready");
+    $("#xen-loader").addClass("removed");
+
     /*
     $("#site-header").css("display", "none!important");
 
@@ -22,33 +25,229 @@
 
     setTimeout(function () {
       $("#site-header").css("opacity", "1");
-    }, 10);
+    }, 1);
 
-    console.log("position: " + $("#site-header").css("position"));
+    /**   FLIP CARDS    */
+    [].slice
+      .call(document.querySelectorAll(".pct-recurso-tile"))
+      .forEach(function (el) {
+        let $el = $(el);
+        let $card = $el.find("flip-card");
+        let $bt = $el.find(".card-content .button");
+        let $close = $el.find(".pct-close-flipped a");
+
+        $bt.on("tap click", function (e) {
+          e.preventDefault();
+          $el.addClass("is-flipped");
+
+          $el
+            .find(".flip-card")
+            .one(
+              "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
+              function (e) {
+                setTimeout(function () {
+                  $el.find(".wpcf7").addClass("showing");
+                  //$el.find(".wpcf7").css("display", "flex");
+                }, 10);
+              }
+            );
+          $el.find('form [type="email"]').focus();
+        });
+
+        $close.on("tap click", function (e) {
+          e.preventDefault();
+          $el.removeClass("is-flipped");
+          $el.find(".flip-card-back").css("backface-visibility", "hidden");
+          $el.find(".wpcf7").removeClass("showing");
+        });
+
+        //console.log($el.attr("class"));
+      });
+
+    /*    let card = document.querySelector(".flip-card");
+    card.addEventListener("click", function () {
+      card.classList.toggle("is-flipped");
+    });*/
   });
 
   /**----------------------- END ONLOAD SECTION ----------------------------------------------------------*/
 
-  /*------------------------------------------------------------------------------------------------------*\
+  /*------------------------------------------------------------------------------------------------------*
+              LEARNDASH GRID ADDON FIXES
+  \*------------------------------------------------------------------------------------------------------*/
+  if ($("#ld_course_list")) {
+    [].slice
+      .call(document.querySelectorAll(".ld_course_grid article"))
+      .forEach(function (el) {
+        let $el = $(el);
+        let $tgt = $el.find("a[rel=bookmark]").first();
+        let $obj = $el.find(".caption h3");
 
+        $tgt.append($obj);
 
+        //console.log($tgt.html());
+      });
+  }
 
-	\*------------------------------------------------------------------------------------------------------*/
+  /*------------------------------------------------------------------------------------------------------*
+              LEARNDASH 3.2 BUGFIX: COURSE CLOSED STILL SHOWING BUTTON TO "enroll now",
+              including with shortcode [learndash_payment_buttons]
+  \*------------------------------------------------------------------------------------------------------*/
+  if ($("body.sfwd-courses-template")) {
+    [].slice
+      .call(document.querySelectorAll("#btn-join"))
+      .forEach(function (el) {
+        let $el = $(el);
+        let url = new URL(window.location);
+        let host = url.host;
+        let btLink = $el.attr("href").split("//")[1]; // eliminamos el protocolo para poder comparar con host
 
-  $(".show_form").on("click tap", function (e) {
-    e.preventDefault();
-    const $bt = $(this);
-    const $tgt = $("#" + $bt.attr("href").split("#")[1]);
+        /*
+        console.log("Host url: " + url.host);
+        console.log("LINK url: " + btLink);
+        console.log("bt parent class = " + $el.parent().attr("class"));
+        */
+        if (host === btLink) {
+          if ($el.parent().attr("class") == "ld-course-status-action") {
+            $el.replaceWith(
+              "<span>Este curso está cerrado temporalmente.</span>"
+            );
+          } else {
+            $el.css("display", "none");
+          }
+        }
+      });
 
-    $bt.addClass("hided");
+    function getLastDirname(url) {
+      let pathsplitted = url.split("/");
+      return pathsplitted[pathsplitted.length - 2];
+    }
 
-    $tgt.slideDown(300, function () {
-      /*const $ipt = $($tgt.find('.wpcf7-form-control-wrap > input')[0]); // primer input de usuario del formulario
-			$ipt.focus();*/
-      $bt.slideUp(100);
+    function getHostName(url) {
+      let pathsplitted = url.split("/");
+      return pathsplitted[pathsplitted.length - 2];
+    }
+  }
+
+  /*------------------------------------------------------------------------------------------------------*
+              FICHA DE PRODUCTO PARA CURSOS ONLINE (COLOCANDO TÍTULO SOBRE IMAGEN)
+  \*------------------------------------------------------------------------------------------------------*/
+
+  if ($("body.single-product")) {
+    //console.log("FICHA DE PRODUCTO");
+    let $tgt = $(
+      ".woocommerce-product-gallery .woocommerce-product-gallery__wrapper"
+    );
+    let $tit = $("h1.product_title");
+
+    $tgt.append($tit);
+
+    /*
+    [].slice
+      .call(document.querySelectorAll(".woocommerce-product-gallery"))
+      .forEach(function (el) {
+        let $el = $(el);
+        let $tgt = $el.find("a[rel=bookmark]").first();
+        let $obj = $el.find(".caption h3");
+
+        $tgt.append($obj);
+
+        //console.log($tgt.html());
+      });
+      */
+  }
+
+  /*------------------------------------------------------------------------------------------------------*
+              LEARNDASH COURSE PAGE: FIX LEARNDASH COURSE HEADER NOT SHOWING BG IMAGE --> BUILD HEADER WITH ELEMENTOR: Need to flip ld-course-status into elementor content
+  \*------------------------------------------------------------------------------------------------------*/
+  /*  Movemos el "course status dentro del contenido del template de elementor"  */
+  if ($("body.sfwd-courses-template")) {
+    //$(".ld-course-status").prepend($(".ld-tab-content > .elementor"));
+    let selector =
+      ".ld-tab-content > [data-elementor-type=wp-post] > .elementor-inner > .elementor-section-wrap > section:nth-of-type(2)";
+
+    if ($(selector)) {
+      $(selector).prepend($(".ld-course-status"));
+    }
+
+    /* Movemos seccion CTA si existe ANTES de el "Contenido del curso"  */
+    if ($("section.pct-bg-cta-a").length) {
+      $(".pct-ld-course-desc > .elementor-column-wrap > div").append(
+        $(".ld-item-list")
+      );
+      //$("#main > .learndash").prepend($("section.pct-bg-cta-a"));
+    }
+  }
+
+  [].slice
+    .call(
+      document.querySelectorAll(
+        "pct-footer-cta .button, .oew-call-to-action .button"
+      )
+    )
+    .forEach(function (el) {
+      let $el = $(el);
+      $el.addClass("elementor-animation-grow");
+      //console.log($el.attr("class"));
     });
-  });
 
+  /*------------------------------------------------------------------------------------------------------*
+              SECCIONES: GENERACIÓN DE BORDES PERSONALIZADOS SUPERIOR E INFERIOR
+  \*------------------------------------------------------------------------------------------------------*/
+  /*
+  [].slice
+    .call(document.querySelectorAll('[class*="section-border"]'))
+    .forEach(function (el) {
+      let $section = $(el);
+
+      let bgcolor = $section.css("background-color")
+        ? $section.css("background-color")
+        : "#fff";
+      let vwBreakMobile = 479; // vw below which we make the top&bottom height smaller...
+      let vw = window.innerWidth;
+      let svgH = vw > vwBreakMobile ? 70 : 35;
+
+      let border;
+
+/
+      if ($section.hasClass("section-border-top-buz-dcha")) {
+        //let svgH = 70;
+        border = {
+          borderPos: "top",
+          svgH: svgH,
+          svgData: `<polygon points="0,0 1000,${svgH} 0,${svgH}" style="fill:${bgcolor};stroke-width:0" />`,
+        };
+        let $border = $section
+          .prepend(applySVG(border))
+          .find('[class*="pct-section-' + border.borderPos + '"]');
+
+        $border.css("top", 0);
+        $border.css("margin-top", -svgH + "px");
+      }
+
+      if ($section.hasClass("section-border-bottom-buz-izq")) {
+        //let svgH = 70;
+        border = {
+          borderPos: "bottom",
+          svgH: svgH,
+          svgData: `<polygon points="0,0 1000,0 0,${svgH}" style="fill:${bgcolor};stroke-width:0" />`,
+        };
+        let $border = $section
+          .prepend(applySVG(border))
+          .find('[class*="pct-section-' + border.borderPos + '"]');
+
+        $border.css("bottom", 0);
+        $border.css("margin-bottom", -svgH + "px");
+      }
+
+      function applySVG(borderDef) {
+        let borderPos = borderDef.borderPos; // string top|bottom
+        let svgH = borderDef.svgH ? borderDef.svgH : 70;
+        let svgData = borderDef.svgData; // svg poligon, paths, etc. => svg content.
+        return `<div class="pct-section-${borderPos}-border"><svg viewBox="0 0 1000 ${svgH}" preserveAspectRatio="none" width="100%" height="${svgH}">${svgData}</svg></div>`;
+      }
+    });
+*/
   /*------------------------------------------------------------------------------------------------------*\
 
 							CONTACT FORM 7 EVENTS
@@ -107,6 +306,7 @@
       }
     });
 
+  /**   CLICK SUBMIT BUTTON  */
   [].slice
     .call(document.querySelectorAll(".wpcf7 .wpcf7-submit"))
     .forEach(function (el) {
@@ -114,11 +314,13 @@
       const $tgt = $el.find("a");
       const modalContent = "#SAC";
 
-      addCloseButton_to_modal(modalContent);
+      //addCloseButton_to_modal(modalContent);
 
       // add Close button to modal
 
-      $el.on("click tap submit", function (e) {
+      $el.on("click tap", function (e) {
+        e.preventDefault();
+        clearTimeout(window.fto);
         $(".ajax-loader").remove();
         var bt = $(this);
 
@@ -126,11 +328,22 @@
           return false;
         }
 
+        bt.css("width", bt.outerWidth()); // fijamos el valor inicial de width para que en el css la animación "to:" funcione...
+
         bt.addClass("active");
 
-        setTimeout(function () {
-          bt.addClass("loader");
-        }, 300);
+        //detectamos final de animacion roundIt para mostrar icono animado...
+        bt.one(
+          "webkitAnimationEnd oanimationend msAnimationEnd animationend",
+          function (e) {
+            bt.find("i").addClass("showIcon");
+          }
+        );
+
+        window.fto = setTimeout(function () {
+          let form = bt.closest("form");
+          form.submit();
+        }, 500);
       });
     });
 
@@ -143,27 +356,26 @@
 
       btsubmit.css({ display: "none" });
 
-      console.log($form.attr("id"));
-
-      btsubmit.removeClass("loader active");
-      btsubmit.text("¡Gracias!@@@@@");
-      btsubmit.addClass("success animated pulse");
-
-      //if ( '3535' == event.detail.contactFormId ) {location = 'http://www.motorvillagemadrid.com/gracias';}
-      //console.log('FORMULARIO ENVIADO: todo OK: From ID--> ' + event.detail.contactFormId);
+      btsubmit.removeClass("active");
+      btsubmit.find("i").removeClass("showIcon");
+      //btsubmit.text("¡Gracias!@@@@@");
+      //btsubmit.addClass("success animated pulse");
     },
     false
   );
 
-  // ERROR DE VALIDACIÓN, ALGÚN CAMPO NO ES CORRECTO O ES OBLIGATORIO Y ESTÁ VACÍO
+  //ERROR DE VALIDACIÓN, ALGÚN CAMPO NO ES CORRECTO O ES OBLIGATORIO Y ESTÁ VACÍO
+
   document.addEventListener(
     "wpcf7invalid",
     function (event) {
       const $form = $("[id^='wpcf7-f" + event.detail.contactFormId + "']");
       const btsubmit = $form.find(".wpcf7-submit");
 
-      btsubmit.removeClass("loader active");
-      btsubmit.text("Enviar de nuevo");
+      btsubmit.removeClass("active");
+      btsubmit.find("i").removeClass("showIcon");
+      //btsubmit.text("Enviar de nuevo");
+      console.log("ERROR DE VALIDACION: " + event.detail.contactFormLocale);
     },
     false
   );
@@ -175,9 +387,61 @@
       const $form = $("[id^='wpcf7-f" + event.detail.contactFormId + "']");
       const btsubmit = $form.find(".wpcf7-submit");
 
-      btsubmit.removeClass("loader active");
+      btsubmit.removeClass("active");
+      btsubmit.find("i").removeClass("showIcon");
+
       btsubmit.text("Volver a enviar");
-      //console.log('NO SE HA PODIDO ENVIAR EL MAIL: ' + event.detail.contactFormLocale);
+      console.log(
+        "NO SE HA PODIDO ENVIAR EL MAIL: " + event.detail.contactFormLocale
+      );
+    },
+    false
+  );
+
+  // OK, PERO EL SERVIDOR NO HA PODIDO ENVIAR EL MAIL. PROBLEMA EN EL SERVIDOR SMTP¿?
+  document.addEventListener(
+    "wpcf7mailfailed",
+    function (event) {
+      const $form = $("[id^='wpcf7-f" + event.detail.contactFormId + "']");
+      const btsubmit = $form.find(".wpcf7-submit");
+
+      btsubmit.removeClass("active");
+      btsubmit.find("i").removeClass("showIcon");
+
+      btsubmit.text("Volver a enviar");
+      //console.log("NO SE HA PODIDO ENVIAR EL MAIL: " + event.detail.contactFormLocale);
+    },
+    false
+  );
+
+  // ERROR, SPAM ACTIVITY
+  document.addEventListener(
+    "wpcf7spam",
+    function (event) {
+      const $form = $("[id^='wpcf7-f" + event.detail.contactFormId + "']");
+      const btsubmit = $form.find(".wpcf7-submit");
+
+      btsubmit.removeClass("active");
+      btsubmit.find("i").removeClass("showIcon");
+
+      //btsubmit.text("Volver a enviar");
+      console.log("SPAM ACTIVITY: " + event.detail.contactFormLocale);
+    },
+    false
+  );
+
+  // OK, PERO EL SERVIDOR NO HA PODIDO ENVIAR EL MAIL. PROBLEMA EN EL SERVIDOR SMTP¿?
+  document.addEventListener(
+    "wpcf7mailfailed",
+    function (event) {
+      const $form = $("[id^='wpcf7-f" + event.detail.contactFormId + "']");
+      const btsubmit = $form.find(".wpcf7-submit");
+
+      btsubmit.removeClass("active");
+      btsubmit.find("i").removeClass("showIcon");
+
+      btsubmit.text("Volver a enviar");
+      //console.log("NO SE HA PODIDO ENVIAR EL MAIL: " + event.detail.contactFormLocale);
     },
     false
   );
@@ -263,6 +527,20 @@
       );
     });
 
+  /*------------------------------------------------------------------------------------------------------*
+                      CONTACT FORM 7 submit_on_email for NEWSLETTERS
+  \*------------------------------------------------------------------------------------------------------*/
+  [].slice
+    .call(document.querySelectorAll("form.submit_on_email"))
+    .forEach(function (el) {
+      let $form = $(el);
+      let $bt_submit = $form.find('button,input[type="submit"]');
+
+      //$bt_submit.append($form.find('[type = "email"]'));
+      $form.find('[type = "email"]').parent().append($bt_submit);
+      //console.log('clientID= ' + $form.find('input.gacid').attr('value'));
+    });
+
   /*------------------------------------------------------------------------------------------------------*\
 
 							CONTACT FORM 7 ADD CLIENT ID from ANALYTICS COOKIE as input hidden for user
@@ -290,42 +568,4 @@
     //console.log(clientID);
     return clientID;
   }
-
-  /* SVG IE EDGE FIX FOR <text> transform (positioning) */
-  [].slice
-    .call(document.querySelectorAll("svg text"))
-    .forEach(function (element) {
-      const txt = $(element);
-      if (
-        txt.css("transform") &&
-        txt.css("transform").indexOf("matrix") !== -1 &&
-        !txt.attr("transform")
-      ) {
-        var newMatrix = txt
-          .css("transform")
-          .replace(/ /g, "")
-          .replace(/,/g, " ");
-        txt.attr({ transform: newMatrix });
-      }
-    });
-
-  // replace links with this string "http://./"   for local base site URL
-  [].slice
-    .call(document.querySelectorAll('[href*="//./"]'))
-    .forEach(function (inputEl) {
-      var $el = $(inputEl);
-      var homeURL = globalObject.homeURL; // via wp_localize_script on functions.php
-
-      $el.attr("href", homeURL + $el.attr("href").replace("http://./", "/"));
-
-      if ($el.attr("onclick")) {
-        var href = $el.attr("onclick");
-        if (href.indexOf("//***") > -1) {
-          $el.attr(
-            "onclick",
-            "window.location='" + homeURL + href.replace(/^.*\/\/[^\/]+/, "")
-          );
-        }
-      }
-    });
 })(jQuery);
