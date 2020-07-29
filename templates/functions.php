7cfebeac6b79795d93004c1f0cc0de448ac8b1de
xenolito <oscar.rey.tajes@gmail.com>
2020-07-29 10:43:17 +0200

    tweaks for another parent theme

--- templates/functions.php
+++ templates/functions.php
@@ -1,19 +1,24 @@
 <?php
-function oceanwp_child_enqueue_parent_style() {
-	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update your theme)
-	$theme   = wp_get_theme( 'OceanWP' );
-	$version = $theme->get( 'Version' );
-	// Load the stylesheet
-	wp_enqueue_style( 'pictau-child', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
-
+add_action('after_setup_theme', 'uncode_language_setup');
+function uncode_language_setup()
+{
+	load_child_theme_textdomain('uncode', get_stylesheet_directory() . '/languages');
 }
-add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style',100 );/* custom PHP functions below this line */
 
-add_action( 'wp_enqueue_scripts', 'remove_default_stylesheet', 20 );
-function remove_default_stylesheet() {
-	wp_deregister_style( 'elementor-icons' );
-	//wp_deregister_style( 'simple-line-icons' ); // used by oceanwp like search magnifier and home icon on header breadcrumb...
+function theme_enqueue_styles()
+{
+	$production_mode = ot_get_option('_uncode_production');
+	$resources_version = ($production_mode === 'on') ? null : rand();
+	if ( function_exists('get_rocket_option') && ( get_rocket_option( 'remove_query_strings' ) || get_rocket_option( 'minify_css' ) || get_rocket_option( 'minify_js' ) ) ) {
+		$resources_version = null;
+	}
+	$parent_style = 'uncode-style';
+	$child_style = array('uncode-style');
+	wp_enqueue_style($parent_style, get_template_directory_uri() . '/library/css/style.css', array(), $resources_version);
+	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', $child_style, $resources_version);
 }
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles', 100);
+
 /*------------------------------------------------------------------------------------------------------*\
 
 						WP MAIL FROM CUSTOMIZATION
