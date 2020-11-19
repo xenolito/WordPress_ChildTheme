6aa0498d499ef624c878031c6a8144879a6ba475
xenolito <oscar.rey.tajes@gmail.com>
2020-11-19 12:48:52 +0100

    fixing minor issues

--- js/custom_theme_scripts.js
+++ js/custom_theme_scripts.js
@@ -1,4 +1,4 @@
-//import { Car } from "./mod/oreyClass";
+import { Car } from "./mod/oreyClass";
 import "./mod/avif-webp";
 
 (function ($) {
@@ -7,7 +7,7 @@ import "./mod/avif-webp";
   /** Global vars defined by functions.php with 'wp_localize_script' @ script enqueue   **/
   var global = pct_globalVars;
 
-  /*  let Micoche = new Car("Toyota", "RAV4");
+  let Micoche = new Car("Toyota", "RAV4");
   Micoche.ponMarcha("D");
   console.log(
     "Marcha: " +
@@ -17,7 +17,7 @@ import "./mod/avif-webp";
       " \nModelo: " +
       Micoche.modelo
   );
-*/
+
   /*------------------------------------------------------------------------------------------------------*\
 
 							ONLOAD
